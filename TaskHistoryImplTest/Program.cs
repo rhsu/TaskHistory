3ded991b8b57a73388cb88adecf5c889269dbc60
robert <rhsu123@gmail.com>
2016-03-15 21:27:21 -0700

    backup

---
+++ TaskHistoryImplTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Ninject;
+using TaskHistoryApi.Tasks;
+using TaskHistoryImpl.Tasks;
+
+namespace TaskHistoryImplTest
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			using (IKernel kernel = new StandardKernel ()) 
+			{
+				kernel.Bind<ITaskRepo> ()
+					.To<TaskRepo> ();
+
+				kernel.Bind<ITask> ()
+					.To<Task> ();
+
+
+				var testDto = new Task (1, "Hello", true);
+				ITaskRepo myRepo = kernel.Get<ITaskRepo> ();
+
+				myRepo.CreateTask (testDto);
+			}
+
+
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
