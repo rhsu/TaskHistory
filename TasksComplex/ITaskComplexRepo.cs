339c627b76237edad840fcbcf28c50cfa52e5a7e
robert <rhsu123@gmail.com>
2016-03-22 13:44:59 -0700

    backup

---
+++ TasksComplex/ITaskComplexRepo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using TaskHistoryApi.Tasks;
+using System.Collections.Generic;
+using TaskHistoryApi.Users;
+
+namespace TaskHistoryApi.TasksComplex
+{
+	public interface ITasksComplex
+	{
+		IEnumerable<ITaskComplex> GetComplexTasksForUser(IUser user);
+	}
+}
