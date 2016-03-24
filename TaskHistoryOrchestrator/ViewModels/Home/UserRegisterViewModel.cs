ec31d82cbff8877c36c1b3c1722fc72c2d87ae9f
robert <rhsu123@gmail.com>
2016-03-24 15:18:17 -0700

    #30

---
+++ TaskHistoryOrchestrator/ViewModels/Home/UserRegisterViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TaskHistoryOrchestrator.ViewModels
+{
+	public class UserRegisterViewModel
+	{
+		public int? UserId { get; }
+		public string Username { get; }
+		public string Password { get; }
+		public string Email { get; }
+
+		public UserRegisterViewModel ()
+		{
+		}
+	}
+}
