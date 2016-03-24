c25a9acc9c6e1a3ab9ef7d11f5a7b047d807b1bf
robert <rhsu123@gmail.com>
2016-03-24 10:42:46 -0700

    fixed the fullName property

--- TaskHistoryImpl/Users/User.cs
+++ TaskHistoryImpl/Users/User.cs
@@ -5,11 +5,13 @@ namespace TaskHistoryImpl
 {
 	public class User : IUser
 	{
+		private readonly string _fullName;
+
 		public int UserId { get; }
 		public string Username { get; }
 		public string FirstName { get; }
 		public string LastName { get; }
-		public string FullName { get { return this.FirstName + " " + this.LastName; } }
+		public string FullName { get { return _fullName; } }
 		public string Email { get; }
 
 		public User (int id, string userName, string firstName, string lastName, string email)
@@ -19,6 +21,8 @@ namespace TaskHistoryImpl
 			FirstName = firstName;
 			LastName = lastName;
 			Email = email;
+
+			_fullName = string.Format ("{0} {1}", FirstName, LastName);
 		}
 	}
 }
