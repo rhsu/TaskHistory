540a8dac982f84e3b520500387722f044b9f5c61
robert <rhsu123@gmail.com>
2016-03-29 15:30:14 -0700

    stuff

---
+++ TaskHistoryObjectMapper/ObjectMapperUser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using TaskHistoryApi.Users;
+using TaskHistoryViewModel.ViewModels;
+using TaskHistoryImpl;
+
+namespace TaskHistoryObjectMapper
+{
+	public class ObjectMapperUser
+	{
+		public IUser Map(UserRegisterViewModel viewModel)
+		{
+			if (viewModel == null)
+				throw new ArgumentNullException("viewModel");
+
+			var userFactory = new UserFactory ();
+			// userFactory
+
+			return null;
+		}
+
+		public ObjectMapperUser ()
+		{
+		}
+	}
+}
+
