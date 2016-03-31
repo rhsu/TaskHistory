b6d9f562423fe2ec254f62c18ed3f3767670a882
robert <rhsu123@gmail.com>
2016-03-31 11:30:45 -0700

    backup

---
+++ TaskHistory.ViewModel/UserSuccessfulRegisteredViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TaskHistory.ViewModel.Users
+{
+	public class UserSuccessfulRegisteredViewModel
+	{
+		public string FirstName { get; }
+		public string Username { get; }
+		public string Email { get; }
+
+		public UserSuccessfulRegisteredViewModel ()
+		{
+		}
+	}
+}
+
