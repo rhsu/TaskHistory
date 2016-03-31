4152d3d00c467e3d467d305e9899c1215ba04806
robert <rhsu123@gmail.com>
2016-03-31 15:00:21 -0700

    unit tests

---
+++ TaskHistory.Impl.Test/TaskRepoTest.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using TaskHistory.Api.Tasks;
+using TaskHistory.Impl.Tasks;
+
+namespace TaskHistory.Impl.Test
+{
+	[TestFixture]
+	public class TaskRepoTest
+	{
+		private ITaskRepo _taskRepo;
+
+		[SetUp]
+		public void Init()
+		{
+			_taskRepo = new TaskRepo ();
+		}
+
+		[Test]
+		public void InsertNewTaskTest ()
+		{
+		}
+
+		[Test]
+		public void DeleteTask ()
+		{
+			
+		}
+
+		[Test]
+		public void UpdateTask ()
+		{
+		}
+
+		[Test]
+		public void GetTasksForUser()
+		{
+		}
+	}
+}
+
