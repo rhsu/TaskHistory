5745e6c4d472c3b85e323fa69364fc9bee1c66cb
robert <rhsu123@gmail.com>
2016-04-20 12:03:45 -0700

    view model fix

--- TaskHistory.ViewModel/UserSuccessfulRegisteredViewModel.cs
+++ TaskHistory.ViewModel/UserSuccessfulRegisteredViewModel.cs
@@ -4,12 +4,28 @@ namespace TaskHistory.ViewModel.Users
 {
 	public class UserSuccessfulRegisteredViewModel
 	{
+		public bool ContainsErrors { get; }
 		public string FirstName { get; }
 		public string Username { get; }
 		public string Email { get; }
 
-		public UserSuccessfulRegisteredViewModel ()
+		public UserSuccessfulRegisteredViewModel()
 		{
+		}
+
+		public UserSuccessfulRegisteredViewModel (bool containsErrors, 
+			string firstName, 
+			string userName, 
+			string email)
+		{
+			this.ContainsErrors = containsErrors;
+
+			if (!containsErrors) 
+			{
+				this.FirstName = firstName;
+				this.Username = userName;
+				this.Email = email;
+			}
 		}
 	}
 }
