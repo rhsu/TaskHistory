6d5f023bdda880d553db276c63d724026fc57a09
robert <rhsu123@gmail.com>
2016-06-18 14:28:42 -0700

    need to debug

---
+++ TaskHistory.Impl.Test/History/HistoryItemTest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using NUnit.Framework;
+using TaskHistory.Api.History;
+using TaskHistory.Impl.History;
+
+namespace TaskHistory.Impl.Test
+{
+	[TestFixture]
+	public class HistoryItemTest
+	{
+		private const int historyId = 12;
+		private readonly DateTime actionDate = new DateTime (2001, 1, 1);
+		private const ActionType actionDone = ActionType.Create;
+		private const BusinessObjectType businessObjType = BusinessObjectType.Labels;
+		private const int userId = 34;
+		private const string userName = "User12";
+
+		private HistoryItem itemUnderTest;
+
+		[SetUp]
+		public void Init()
+		{
+			itemUnderTest = new HistoryItem (historyId,
+				actionDate,
+				actionDone,
+				businessObjType,
+				userId,
+				userName);
+		}
+
+		[Test]
+		public void ConstructorTest()
+		{
+			Assert.NotNull (itemUnderTest);
+		}
+			
+		[Test]
+		public void HistoryIdAccTest()
+		{
+			Assert.Equals (itemUnderTest.HistoryId, historyId);
+		}
+
+		[Test]
+		public void ActionDateAccTest()
+		{
+			Assert.Equals (itemUnderTest.ActionDate, actionDate);
+		}
+
+		[Test]
+		public void ActionDoneAccTest()
+		{
+			Assert.Equals (itemUnderTest.ActionDone, actionDone);
+		}
+
+		[Test]
+		public void BusinessObjectAccTest()
+		{
+			Assert.Equals (itemUnderTest.BusinessObject, businessObjType);
+		}
+
+		[Test]
+		public void UserIdAccTest()
+		{
+			Assert.Equals (itemUnderTest.UserId, userId);
+		}
+
+		[Test]
+		public void UserNameAccTest()
+		{
+			Assert.Equals (itemUnderTest.UserName, userName);
+		}
+	}
+}
+
---
+++ TaskHistory.Impl/History/HistoryItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using TaskHistory.Api.History;
+
+namespace TaskHistory.Impl.History
+{
+	public class HistoryItem : IHistoryItem
+	{
+		public int HistoryId { get; }
+		public DateTime ActionDate { get; }
+		public ActionType ActionDone { get; }
+		public BusinessObjectType BusinessObject { get; }
+		public int UserId { get; }
+		public string UserName { get; }
+
+		public HistoryItem (int historyId,
+			DateTime actionDate,
+			ActionType actionDone,
+			BusinessObjectType businessObjType,
+			int userId,
+			string userName)
+		{
+			HistoryId = historyId;
+			ActionDate = actionDate;
+			ActionDone = actionDone;
+			BusinessObject = businessObjType;
+			UserId = userId;
+			UserName = userName;
+		}
+	}
+}
+
