ecf1f851f666046dd5d523672226500aa8118c85
robert <rhsu123@gmail.com>
2016-07-11 07:55:34 -0700

    idea for terminal orchestrator

---
+++ TaskHistory.Orchestrator/TerminalOrchestrator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TaskHistoryOrchestrator
+{
+	public class TerminalOrchestrator
+	{
+		public TerminalOrchestrator ()
+		{
+		}
+
+		public string ProcessCommand(string command)
+		{
+			return "Response string";
+		}
+	}
+}
+
