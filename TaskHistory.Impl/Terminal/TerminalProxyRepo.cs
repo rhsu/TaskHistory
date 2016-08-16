cff2d1e83ffd94c136c7a7f51ba760eded1e1543
robert <rhsu123@gmail.com>
2016-08-16 16:09:10 -0700

    and closer

--- TaskHistory.Impl/Terminal/TerminalProxyRepo.cs
+++ TaskHistory.Impl/Terminal/TerminalProxyRepo.cs
@@ -45,34 +45,71 @@ namespace TaskHistory.Impl.Terminal
 			return new List<ITerminalObject>();
 		}
 
-		IEnumerable<ITerminalObject> PerformReadOperation(TerminalCommandOption commandOption, TerminalRegisteredObject registeredObject)
+		IEnumerable<ITerminalObject> PerformReadOperation(TerminalCommandOption commandOption, TerminalRegisteredObject registeredObject, IUser user)
 		{
+			if (user == null)
+				throw new ArgumentNullException ("user");
+
 			if (registeredObject == TerminalRegisteredObject.Error)
 				throw new ArgumentOutOfRangeException ("registeredObject", "The registered object is of type Error");
 
+			//TODO refactor this switch statement into a function which transofmrs an enum + IUser into a collection of ITerminalObjects
 			switch (registeredObject) 
 			{
 			case TerminalRegisteredObject.Label:
-				var labels = _labelRepo.ReadAllLabelsForUser (null);
+				var labels = _labelRepo.ReadAllLabelsForUser (user);
+				//TODO some service that converts label to ITerminalObject
+				//TODO consider adding in an attribute onto Label to denote that field x is the Id and field y is the Value
+				//TODO should that attribute be on ILabel or Label?
 
 				break;
 			case TerminalRegisteredObject.Task:
+				var tasks = _taskRepo.ReadTasksForUser (user);
+
 				break;
 			}
+
+			var returnVal = new List<ITerminalObject> ();
+			return returnVal;
 		}
 
-		public int PerformInsertOperation(TerminalCommandOption commandOption, TerminalRegisteredObject registeredObject)
+		public int PerformInsertOperation(TerminalCommandOption commandOption, TerminalRegisteredObject registeredObject, IUser user)
 		{
+			if (user == null)
+				throw new ArgumentNullException ("user");
+
+			if (registeredObject == TerminalRegisteredObject.Error)
+				throw new ArgumentOutOfRangeException ("registeredObject", "The registered object is of type Error");
+			
+			switch (registeredObject) 
+			{
+			case TerminalRegisteredObject.Label:
+				//_labelRepo.UpdateLabel(someId)
+				//TODO permissions. For now we can just ensure that the stored procedure deletes based off of a where clause looking up UserId
+				//TODO need to pass in Id
+				break;
+			case TerminalRegisteredObject.Task:
+				
+
+				break;
+			}
+
 			return -1;
 		}
 
 		int PerformUpdateOperation(TerminalCommandOption commandOption, TerminalRegisteredObject registeredObject)
 		{
+			if (registeredObject == TerminalRegisteredObject.Error)
+				throw new ArgumentOutOfRangeException ("registeredObject", "The registered object is of type Error");
+
 			return -1;
 		}
 
 		int PerformDeleteOperation(TerminalCommandOption commandOption, TerminalRegisteredObject registeredObject)
 		{
+			if (registeredObject == TerminalRegisteredObject.Error)
+				throw new ArgumentOutOfRangeException ("registeredObject", "The registered object is of type Error");
+
 			return -1;
 		}
 
