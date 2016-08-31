0cdb3e24be362317c6e7aa6fce700243f740fb84
robert <rhsu123@gmail.com>
2016-08-31 15:59:58 -0700

    speced out the ProxyRepo

--- TaskHistory.Api/Terminal/ITerminalProxyRepo.cs
+++ TaskHistory.Api/Terminal/ITerminalProxyRepo.cs
@@ -6,16 +6,42 @@ namespace TaskHistory.Api.Terminal
 {
 	public interface ITerminalProxyRepo
 	{
+		/// <summary>
+		/// Performs the create operation.
+		/// </summary>
+		/// <returns>Number of Records Created</returns>
+		/// <param name="commandOption">Command option.</param>
+		/// <param name="registeredObject">Registered object.</param>
+		/// <param name="user">User.</param>
 		int PerformCreateOperation(TerminalCommandOption commandOption, 
 			TerminalRegisteredObject registeredObject,
 			IUser user);
 
-		/* IEnumerable<ITerminalObject> PerformReadOperation(TerminalCommandOption commandOption, 
+		/// <summary>
+		/// Performs the read operation.
+		/// </summary>
+		/// <returns>The read ITerminalObjects</returns>
+		/// <param name="commandOption">Command option.</param>
+		/// <param name="registeredObject">Registered object.</param>
+		/// <param name="user">User.</param>
+		IEnumerable<ITerminalObject> PerformReadOperation(TerminalCommandOption commandOption, 
 			TerminalRegisteredObject registeredObject, 
 			IUser user);
 
+		/// <summary>
+		/// Performs the update operation.
+		/// </summary>
+		/// <returns>Number of Records Created</returns>
+		/// <param name="commandOption">Command option.</param>
+		/// <param name="registeredObject">Registered object.</param>
 		int PerformUpdateOperation (TerminalCommandOption commandOption, TerminalRegisteredObject registeredObject);
 
-		int PerformDeleteOperation (TerminalCommandOption commandOption, TerminalRegisteredObject registeredObject); */
+		/// <summary>
+		/// Performs the delete operation.
+		/// </summary>
+		/// <returns>Number of Records Created</returns>
+		/// <param name="commandOption">Command option.</param>
+		/// <param name="registeredObject">Registered object.</param>
+		int PerformDeleteOperation (TerminalCommandOption commandOption, TerminalRegisteredObject registeredObject);
 	}
 }
