843c64dd47b4cd4f298068b98a61d94ad84bb9f0
robert <rhsu123@gmail.com>
2016-10-07 16:11:22 -0700

    added in mvc project scaffold

---
+++ AngularProto/Global.asax.cs
@@ -0,0 +1,15 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace AngularProto
+{
+	public class Global : HttpApplication
+	{
+		protected void Application_Start()
+		{
+			AreaRegistration.RegisterAllAreas();
+			RouteConfig.RegisterRoutes(RouteTable.Routes);
+		}
+	}
+}
