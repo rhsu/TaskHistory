b1126bdaa98ae691cf796bfa3ac55544e6c78461
robert <rhsu123@gmail.com>
2016-10-08 09:21:25 -0700

    more scaffolding

--- AngularProto/Controllers/HomeController.cs
+++ AngularProto/Controllers/HomeController.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
-using System.Web.Mvc.Ajax;
 
 namespace AngularProto.Controllers
 {
@@ -11,12 +7,6 @@ namespace AngularProto.Controllers
 	{
 		public ActionResult Index()
 		{
-			var mvcName = typeof(Controller).Assembly.GetName();
-			var isMono = Type.GetType("Mono.Runtime") != null;
-
-			ViewData["Version"] = mvcName.Version.Major + "." + mvcName.Version.Minor;
-			ViewData["Runtime"] = isMono ? "Mono" : ".NET";
-
 			return View();
 		}
 	}
