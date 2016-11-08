6a3cfe4bddfe6fe6126df734cbd9e2c52ba82c6c
robert <rhsu123@gmail.com>
2016-11-08 14:14:15 -0800

    fixed some spacing issues with AboutController

--- AngularProto/Controllers/AboutController.cs
+++ AngularProto/Controllers/AboutController.cs
@@ -3,17 +3,17 @@ using System.Web.Mvc;
 
 namespace AngularProto.Controllers
 {
-    public class AboutController : Controller
-    {
-        public ActionResult Index()
-        {
-			var mvcName = typeof(Controller).Assembly.GetName ();
-			var isMono = Type.GetType ("Mono.Runtime") != null;
+	public class AboutController : Controller
+	{
+		public ActionResult Index()
+		{
+			var mvcName = typeof(Controller).Assembly.GetName();
+			var isMono = Type.GetType("Mono.Runtime") != null;
 
-			ViewData ["Version"] = mvcName.Version.Major + "." + mvcName.Version.Minor;
-			ViewData ["Runtime"] = isMono ? "Mono" : ".NET";
+			ViewData["Version"] = mvcName.Version.Major + "." + mvcName.Version.Minor;
+			ViewData["Runtime"] = isMono ? "Mono" : ".NET";
 
-            return View ();
-        }
-    }
+			return View();
+		}
+	}
 }
