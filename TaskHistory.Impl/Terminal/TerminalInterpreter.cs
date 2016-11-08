b922fde904fc41a432eea212951fb440747a1bdc
robert <rhsu123@gmail.com>
2016-11-08 14:23:18 -0800

    naming conventions

--- TaskHistory.Impl/Terminal/TerminalInterpreter.cs
+++ TaskHistory.Impl/Terminal/TerminalInterpreter.cs
@@ -10,11 +10,11 @@ namespace TaskHistory.Impl.Terminal
 	public class TerminalInterpreter : ITerminalInterpreter
 	{
 		private readonly ITerminalProxyRepo _terminalProxyRepo;
-		const string _default_display_message = "Please Enter a Command. (Type HELP for more options)";
+		const string DefaultDisplayMessage = "Please Enter a Command. (Type HELP for more options)";
 
 		public string GetDefaultDisplayMessage()
 		{
-			return _default_display_message;
+			return DefaultDisplayMessage;
 		}
 
 		public string TranslateResponseToString (string requestCommand, IUser user)
@@ -30,7 +30,7 @@ namespace TaskHistory.Impl.Terminal
 			//TODO Test me if tokenizedString is less than 2 words, then ...
 			string[] tokenizedString = requestCommand.ToUpper ().Trim ().Split (' ');
 			if (tokenizedString.Length < 2)
-				return $"{requestCommand} is invalid. " + _default_display_message;
+				return $"{requestCommand} is invalid. " + DefaultDisplayMessage;
 
 			//2. Get the first word of the request command
 			string firstWord = tokenizedString[0];
@@ -38,14 +38,14 @@ namespace TaskHistory.Impl.Terminal
 			// TODO Test all the possible strings and one thing that is not valid
 			TerminalCommandAction commandAction = TerminalInterpreterHelper.DetermineTerminalCommandAction(firstWord);
 			if (commandAction == TerminalCommandAction.Error)
-				return $"{commandAction} is invalid " + _default_display_message;
+				return $"{commandAction} is invalid " + DefaultDisplayMessage;
 
 			//3 get the second word of the request command
 			string secondWord = tokenizedString[1];
 			// --it should be label, task, or user
 			TerminalRegisteredObject registeredObject = TerminalInterpreterHelper.DetermineTerminalRegisteredObject(secondWord);
 			if (registeredObject == TerminalRegisteredObject.Error)
-				return $"{registeredObject} is invalid " + _default_display_message;
+				return $"{registeredObject} is invalid " + DefaultDisplayMessage;
 
 			//TODO Integrate the two checks. For example "Pig Latin" will return Pig is not a valid action. Latin is not a valid object
 
