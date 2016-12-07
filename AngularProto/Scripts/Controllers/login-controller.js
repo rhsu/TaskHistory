44721f4b9bf6c91ec45320651707f2add06e8cfa
robert <rhsu123@gmail.com>
2016-12-06 16:24:29 -0800

    ghetto styles

--- AngularProto/Scripts/Controllers/login-controller.js
+++ AngularProto/Scripts/Controllers/login-controller.js
@@ -1,15 +1,20 @@
 ﻿(function () {
+	console.log('this is registered');
 	var app = angular.module('app');
 
-	app.controller('LoginController', function ($scope, UserLoginService) {
-		$scope = {
-			data : {},
-			fns : {}
-		};
+	app.controller('LoginController', function ($scope) {
 
-		$scope.fns.login = function () {
-			UserLoginService.login($scope.data.user);
-		}
+		$scope.formData = {};
+		$scope.formData.user = '';
+		$scope.formData.password = '';
+
+		/*$scope.fns.login = function () {
+			/*UserLoginService.login($scope.formData).then(function (response) {
+				
+			}, function (reason) {
+				
+			});
+		};*/
 	});
 
 })();
