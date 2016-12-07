acf9a23ea692c9a01efa58d54ba4fc0dbfb208b5
robert <rhsu123@gmail.com>
2016-12-06 16:41:21 -0800

    backup

--- AngularProto/Scripts/Controllers/login-controller.js
+++ AngularProto/Scripts/Controllers/login-controller.js
@@ -2,11 +2,13 @@
 	console.log('this is registered');
 	var app = angular.module('app');
 
-	app.controller('LoginController', function ($scope) {
+	app.controller('LoginController', function ($scope, UserLoginService) {
 
 		$scope.formData = {};
 		$scope.formData.user = '';
 		$scope.formData.password = '';
+
+		UserLoginService.test();
 
 		/*$scope.fns.login = function () {
 			/*UserLoginService.login($scope.formData).then(function (response) {
--- AngularProto/Scripts/Services/user-login-service.js
+++ AngularProto/Scripts/Services/user-login-service.js
@@ -2,6 +2,19 @@
 	var app = angular.module('app');
 
 	app.factory('UserLoginService', function ($http) {
-		
+		return {
+			test() {
+				console.log('worked');
+			},
+
+			loginUser(loginVM) {
+				/*$http.post('').then(function (response) {
+					
+				}), function (reason) {
+
+				});*/
+			}
+		}
+
 	}); 
 })();
