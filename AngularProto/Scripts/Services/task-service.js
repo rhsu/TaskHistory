fa5eb4f18eb72ceb8303c192a7e95e4727ad24ad
robert <rhsu123@gmail.com>
2016-12-15 14:14:45 -0800

    style changed

--- AngularProto/Scripts/Services/task-service.js
+++ AngularProto/Scripts/Services/task-service.js
@@ -1,52 +1,13 @@
 ﻿(function () {
 	const app = angular.module('app');
 
-	console.log("task service is registered");
-
 	app.factory('TaskService', function ($http) {
-		var tasks = [];
-		var returnObj = {}
-
-		/*returnObj.initTasks = returnObj.refreshTasks().then(function () {
-			return tasks;
-		}, function (reason) {
-			//placeholder here for error handling...
-		});*/
-
-		returnObj.getTasks = function () {
-			return $http.post('/Tasks/GetTasks/');
-			/*then (function (response) {
-				//console.log(response);
-				//tasks = response.data;
-				return response.data;
-			}, function (reason) {
-				//placeholder here for error handling...
-			});*/
-		};
-
-		/*returnObj.getTasks = function () { return tasks; };
-
-		returnObj.initTasks = returnObj.refreshTasks();*/
-
-
-		return returnObj;
-		/*return {
-
-			initTasks = refreshTasks().then(function () {
-				return tasks;
-			}, function (reason) {
-				//placeholder here for error handling...
-			}),
-
-			refreshTasks() {
-				return $http.post('/Tasks/GetTasks/')
-				.then(function (response) {
-					response.data = tasks;
-				}, function (reason) {
-					//placeholder here for error handling...
-				});
+		
+		return {
+			getTasks() {
+				return $http.post('/Tasks/GetTasks/');
 			}
-		}*/
+		}
 	});
 
 })();
