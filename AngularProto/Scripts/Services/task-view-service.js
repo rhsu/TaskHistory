57f91304530b479c0a0b8f3cb1332360939ac57c
robert <rhsu123@gmail.com>
2016-12-18 17:56:24 -0800

    committing for backup

---
+++ AngularProto/Scripts/Services/task-view-service.js
@@ -0,0 +1,28 @@
+﻿(function() {
+	const app = angular.module('app');	
+
+	//TODO: I would like to refactor this to something that indicates that it
+	//      is responsible to creating a {model} while invoking the {task-service}
+	//		What is an appropriate name for task-service to indicate
+	//		that is talks to the server via $http (like a repo).
+	//		Another thing we can do is simply differentiate these by folder structure
+	app.factory('TaskViewService', function (TaskService) {
+
+		return {
+			getTasksForTableView() {
+				getTasks().then(function (response) {
+					if (response.data) {
+						var retVal = [];
+
+						// TODO what is the ECMASCRIPT 6 way of doing this
+						for (var i = 0; i < response.data.length; i++) {
+							
+						}
+					}
+				}, function (reason) {
+					// TODO placeholder for error handling
+				});
+			}
+		}
+	});
+})();
