575505e1a1c158a391f120d4f31b3d03a7e299dc
robert <rhsu123@gmail.com>
2016-12-20 17:46:02 -0800

    removed TODO

--- AngularProto/Scripts/Services/task-service.js
+++ AngularProto/Scripts/Services/task-service.js
@@ -8,7 +8,6 @@
 				return $http.post('/Tasks/GetTasks/');
 			},
 
-			// TODO I don't like passing the whole object. Should just pass what is necessary at min.
 			insertTask(content) {
 				return $http.post('/Tasks/CreateTask/', { content: content });
 			},
