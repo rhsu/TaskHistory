1afa7d594c37085363ed2a5aeee515aadbbf0b81
robert <rhsu123@gmail.com>
2016-12-26 15:52:50 -0800

    backup

---
+++ TaskHistory.Api/Lists/IListRepo.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace TaskHistory.Api
+{
+	public interface IListRepo
+	{
+		IEnumerable<IList> ReadLists(int userId);
+
+		bool UpdateList(int userId, int listId);
+	}
+}
