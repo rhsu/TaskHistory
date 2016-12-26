2ed202a8687f4d916ac38c8005d3cd2af2152c0c
robert <rhsu123@gmail.com>
2016-12-26 15:48:40 -0800

    Interfaces

---
+++ TaskHistory.Api/Lists/IList.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using TaskHistory.Api.Tasks;
+
+namespace TaskHistory.Api.Lists
+{
+	public class IList
+	{
+		int ListId { get; }
+		IEnumerable<ITask> Tasks { get; }
+	}
+}
