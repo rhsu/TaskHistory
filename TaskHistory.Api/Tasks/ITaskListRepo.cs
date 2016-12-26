631acef7924ce9814b9e0718865340b6a461ad89
robert <rhsu123@gmail.com>
2016-12-26 15:52:28 -0800

    annotated interface WARNING: will not build

---
+++ TaskHistory.Api/Tasks/ITaskListRepo.cs
@@ -0,0 +1,15 @@
+﻿namespace TaskHistory.Api.Tasks
+{
+	public interface ITaskListRepo
+	{
+		// TODO not sure if I will need this
+		//IEnumerable<ITask> ReadTasksForList();
+
+		// TODO not sure if I will need this
+		//IEnumerable<ITask> ReadTasksForUser();
+
+		ITask CreateTask(int userId, int taskId, string content);
+
+		ITask UpdateTask(int userId, int taskId, TaskUpdatingParameters updateParams);
+	}
+}
