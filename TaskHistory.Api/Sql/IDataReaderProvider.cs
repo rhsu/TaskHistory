e29d651b76592e432bd71cd17a4e2ba6b2f27706
robert <rhsu123@gmail.com>
2016-12-26 20:45:28 -0800

    backup

--- TaskHistory.Api/Sql/IDataReaderProvider.cs
+++ TaskHistory.Api/Sql/IDataReaderProvider.cs
@@ -2,8 +2,19 @@
 
 namespace TaskHistory.Api.Sql
 {
+	/// <summary>
+	/// Provider for Reading Data From a SQL database
+	/// </summary>
 	public interface IDataReaderProvider
 	{
+		/// <summary>
+		/// Executes a data reader, reading only 1 type
+		/// </summary>
+		/// <returns>The reader for single type.</returns>
+		/// <param name="factory">Factory.</param>
+		/// <param name="storedProcedureName">Stored procedure name.</param>
+		/// <param name="parameter">Parameter.</param>
+		/// <typeparam name="T">The 1st type parameter.</typeparam>
 		T ExecuteReaderForSingleType<T> (IFromDataReaderFactory<T> factory,
 			string storedProcedureName,
 			ISqlDataParameter parameter);
