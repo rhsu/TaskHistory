f3e84029241fa8821234b96c33d4fff265986403
robert <rhsu123@gmail.com>
2017-01-09 08:48:05 -0800

    got something to work

---
+++ TaskHistory.Impl.Test/Users/UserRepoTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using TaskHistory.Api.Users;
+using TaskHistory.Impl.Users;
+
+namespace TaskHistory.Impl.Test
+{
+	[TestFixture]
+	public class UserRepoTest
+	{
+		IUserRepo _userRepo;
+
+		public UserRepoTest()
+		{
+			var userFactory = new UserFactory();
+			var appDataProxy = new ApplicationDataProxyFactory().Build();
+
+			_userRepo = new UserRepo(userFactory, appDataProxy);
+		}
+
+		[Test]
+		public void test_register_user()
+		{
+			// TODO: make username a GUID of length 32?
+			var userParams = new UserRegistrationParameters("username",
+															"password",
+															"first",
+															"last",
+															"email");
+
+			_userRepo.RegisterUser(userParams);
+		}
+	}
+}
