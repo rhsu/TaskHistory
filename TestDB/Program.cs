6b0d00019fdcca8194ea41b88af6964f54cd3c14
robert <rhsu123@gmail.com>
2017-01-08 18:25:53 -0800

    seems to work

---
+++ TestDB/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using TaskHistory.Api.Tasks;
+using TaskHistory.Impl.Configuration;
+using TaskHistory.Impl.Sql;
+using TaskHistory.Impl.Tasks;
+
+namespace TestDB
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			var config = new ConfigurationProvider();
+
+			Console.WriteLine(config.SqlConnectionString);
+		}
+	}
+}
