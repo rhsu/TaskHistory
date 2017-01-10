fed4ce5dd55db6545be4819225b17a2d2d0f31f2
robert <rhsu123@gmail.com>
2017-01-09 21:38:47 -0800

    unit tests done

--- TaskHistory.Impl.Test/Users/UserRepoTest.cs
+++ TaskHistory.Impl.Test/Users/UserRepoTest.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 using TaskHistory.Api.Users;
 using TaskHistory.Impl.Users;
 
@@ -18,16 +19,86 @@ namespace TaskHistory.Impl.Test
 		}
 
 		[Test]
-		public void test_register_user()
+		public void RegisterUser_Works()
 		{
-			// TODO: make username a GUID of length 32 so test won't flap when ran multiple times?
-			var userParams = new UserRegistrationParameters("username",
-															"password",
-															"first",
-															"last",
-															"email");
+			var time = DateTime.UtcNow.ToString("yyyyMMddHHmmssffff");
+
+			var username = $"u{time}";
+			var password = "password";
+			var firstName = "first";
+			var lastName = "last";
+			var email = "email@email.com";
+
+			var userParams = new UserRegistrationParameters(username, 
+			                                                password, 
+			                                                firstName, 
+			                                                lastName, 
+			                                                email);
+
+			IUser newUser = _userRepo.RegisterUser(userParams);
+
+			Assert.NotNull(newUser);
+			Assert.AreEqual(newUser.Username, username);
+			// TODO: Once I can execute select queries against the database, 
+			//       then, do the below assert
+			// Assert.AreEqual(newUser.Password, password);
+			Assert.AreEqual(newUser.FirstName, firstName);
+			Assert.AreEqual(newUser.LastName, lastName);
+			Assert.AreEqual(newUser.Email, email);
+		}
+
+		public void RegisterUser_Fails()
+		{
+			var time = DateTime.UtcNow.ToString("yyyyMMddHHmmssffff");
+
+			var username = $"u{time}";
+			var password = "password";
+			var firstName = "first";
+			var lastName = "last";
+			var email = "email@email.com";
+
+			var userParams = new UserRegistrationParameters(username,
+															password,
+															firstName,
+															lastName,
+															email);
 
 			_userRepo.RegisterUser(userParams);
+
+			IUser sameUser = _userRepo.RegisterUser(userParams);
+			Assert.Null(sameUser);
+		}
+
+		public void ValidateUsernameAndPassword_Works()
+		{
+			var time = DateTime.UtcNow.ToString("yyyyMMddHHmmssffff");
+
+			var username = $"u{time}";
+			var password = "password";
+			var firstName = "first";
+			var lastName = "last";
+			var email = "email@email.com";
+
+			var userParams = new UserRegistrationParameters(username,
+															password,
+															firstName,
+															lastName,
+															email);
+
+			IUser registeredUser = _userRepo.RegisterUser(userParams);
+			IUser validatedUser = _userRepo.ValidateUsernameAndPassword(username, password);
+
+			Assert.NotNull(validatedUser);
+			Assert.AreEqual(registeredUser.UserId, validatedUser.UserId);
+			Assert.AreEqual(registeredUser.FirstName, validatedUser.FirstName);
+			Assert.AreEqual(registeredUser.LastName, validatedUser.LastName);
+			Assert.AreEqual(registeredUser.Username, validatedUser.Username);
+		}
+
+		public void ValidateUsernameAndPassword_Fails()
+		{
+			IUser registeredUser = _userRepo.ValidateUsernameAndPassword("nonsense", "nonsense");
+			Assert.Null(registeredUser);
 		}
 	}
 }
