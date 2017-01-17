e32409ed31ca86f289bd715d0e0f01fd06f5494a
robert <rhsu123@gmail.com>
2017-01-17 08:14:37 -0800

    backup

--- TaskHistory.Impl.Test/TaskLists/TaskListRepoTest.cs
+++ TaskHistory.Impl.Test/TaskLists/TaskListRepoTest.cs
@@ -1,21 +1,36 @@
 ﻿using NUnit.Framework;
+using TaskHistory.Api.TaskLists;
+using TaskHistory.Api.Users;
+using TaskHistory.Impl.TaskLists;
+using TaskHistory.Impl.Users;
 
 namespace TaskHistory.Impl.Test
 {
 	[TestFixture]
 	public class TaskListRepoTest
 	{
+		ITaskListRepo _taskListRepo;
+
 		public TaskListRepoTest()
 		{
+			var factory = new TaskListFactory();
+			var appDataProxy = new ApplicationDataProxyFactory().Build();
+
+			_taskListRepo = new TaskListRepo(factory, appDataProxy);
 		}
 
-		public void AssociateTaskToList()
+		/*public void AssociateTaskToList()
 		{
-		}
+		}*/
 
 		public void Create()
 		{
-			
+			var userFactory = new UserFactory();
+			var appDataProxy = new ApplicationDataProxyFactory().Build();
+
+			IUserRepo _userRepo = new UserRepo(userFactory, appDataProxy);
+
+			// IUser user = _userRepo.RegisterUser
 		}
 
 		/*public void Read()
