74c9819e6c91db4cd10a6f32ad7c4875d594c14e
robert <rhsu123@gmail.com>
2017-01-20 15:16:22 -0800

    done

---
+++ TaskHistory.Impl.Test/FeatureFlags/FeatureFlagRepoTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using TaskHistory.Api.FeatureFlags;
+using TaskHistory.Impl.FeatureFlags;
+
+namespace TaskHistory.Impl.Test
+{
+	[TestFixture]
+	public class FeatureFlagRepoTest
+	{
+		IFeatureFlagRepo _repo;
+		TestFixtures _testFixtures;
+
+		public FeatureFlagRepoTest()
+		{
+			var factory = new FeatureFlagFactory();
+			var dataProxy = new ApplicationDataProxyFactory();
+
+			_repo = new FeatureFlagRepo(factory, dataProxy);
+			_testFixtures = new TestFixtures();
+		}
+
+		[Test]
+		public void Create()
+		{
+			
+		}
+	}
+}
