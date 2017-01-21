43a87c993bf486229ed9984e54d43ef453076be4
robert <rhsu123@gmail.com>
2017-01-20 21:47:46 -0800

    some scaffolding

---
+++ AngularProto/Controllers/FeatureFlagsController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace AngularProto.Controllers
+{
+	public class FeatureFlagsController : Controller
+	{
+		[HttpGet]
+		public ActionResult Index()
+		{
+			return View();
+		}
+	}
+}
