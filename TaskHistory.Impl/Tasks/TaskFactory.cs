dce762c123dd12dfe766c3ac29331e79f42ca00d
robert <rhsu123@gmail.com>
2017-01-20 21:04:32 -0800

    done

--- TaskHistory.Impl/Tasks/TaskFactory.cs
+++ TaskHistory.Impl/Tasks/TaskFactory.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using TaskHistory.Api.Sql;
 using TaskHistory.Api.Tasks;
-using TaskHistory.Impl.Sql;
 
 namespace TaskHistory.Impl.Tasks
 {
@@ -10,13 +9,13 @@ namespace TaskHistory.Impl.Tasks
 		public ITask Build(ISqlDataReader reader)
 		{
 			if (reader == null)
-				throw new ArgumentNullException (nameof(reader));
-			
-			int taskId = reader.GetInt ("TaskId");
-			string content = reader.GetString ("Content");
-			bool isCompleted = reader.GetBool ("IsCompleted");
+				throw new ArgumentNullException(nameof(reader));
 
-			return new Task (taskId, content, isCompleted);
+			int taskId = reader.GetInt("TaskId");
+			string content = reader.GetString("Content");
+			bool isCompleted = reader.GetBool("IsCompleted");
+
+			return new Task(taskId, content, isCompleted);
 		}
 	}
 }
