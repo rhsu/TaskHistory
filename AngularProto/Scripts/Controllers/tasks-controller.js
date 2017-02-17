04d8d240c855e9ae2c8c77aedb9927d408ccbe19
robert <rhsu123@gmail.com>
2017-02-16 21:30:32 -0800

    fixing indents

--- AngularProto/Scripts/Controllers/tasks-controller.js
+++ AngularProto/Scripts/Controllers/tasks-controller.js
@@ -1,57 +1,57 @@
 (function () {
-	const app = angular.module('app');
+    const app = angular.module('app');
 
-	app.controller('TasksController', function ($scope, TaskTableViewService) {
-		$scope.pageData = {};
-		$scope.pageData.tasks = [];
+    app.controller('TasksController', function ($scope, TaskTableViewService) {
+        $scope.pageData = {};
+        $scope.pageData.tasks = [];
 
-		$scope.formData = {};
-		$scope.formData.taskContent = '';
+        $scope.formData = {};
+        $scope.formData.taskContent = '';
 
-		$scope.pageFns = {};
+        $scope.pageFns = {};
 
-		var refreshTasks = function () {
-			TaskTableViewService.retrieve().then(function (tasks) {
-				$scope.pageData.tasks = tasks;
-			}, function (reason) {});
-		};
+        var refreshTasks = function () {
+            TaskTableViewService.retrieve().then(function (tasks) {
+                $scope.pageData.tasks = tasks;
+            }, function (reason) {});
+        };
 
-		var resetForm = function () {
-			$scope.formData.taskContent = '';
-		};
+        var resetForm = function () {
+            $scope.formData.taskContent = '';
+        };
 
-		refreshTasks();
+        refreshTasks();
 
-		$scope.pageFns.refreshTasks = function () {
-			refreshTasks();
-		};
+        $scope.pageFns.refreshTasks = function () {
+            refreshTasks();
+        };
 
-		$scope.pageFns.insertTask = function () {
-			TaskTableViewService.create($scope.formData)
-				.then(function (response) {
-					if (response) {
-						resetForm();
-						$scope.pageData.tasks.push(response);
-					}
-				}, function (reason) {});
-		};
+        $scope.pageFns.insertTask = function () {
+            TaskTableViewService.create($scope.formData)
+                .then(function (response) {
+                    if (response) {
+                        resetForm();
+                        $scope.pageData.tasks.push(response);
+                    }
+                }, function (reason) {});
+        };
 
-		$scope.pageFns.deleteTask = function (task) {
+        $scope.pageFns.deleteTask = function (task) {
             task.isDeleted = true;
-			TaskTableViewService.delete(task);
-		};
+            TaskTableViewService.delete(task);
+        };
 
-		$scope.pageFns.undoDeleteTask = function (task) {
+        $scope.pageFns.undoDeleteTask = function (task) {
             task.isDeleted = false;
-			TaskTableViewService.undoDelete(task);
-		};
+            TaskTableViewService.undoDelete(task);
+        };
 
-		$scope.pageFns.displayBackButton = function (task) {
-			return (task.editorState === 'confirmDelete' || task.editorState === 'editing');
-		};
+        $scope.pageFns.displayBackButton = function (task) {
+            return (task.editorState === 'confirmDelete' || task.editorState === 'editing');
+        };
 
-		$scope.pageFns.displayReadonlyMode = function (task) {
-			return (task.editorState !== 'deleted' && task.editorState !== 'editing');
-		};
-	});
+        $scope.pageFns.displayReadonlyMode = function (task) {
+            return (task.editorState !== 'deleted' && task.editorState !== 'editing');
+        };
+    });
 })();
