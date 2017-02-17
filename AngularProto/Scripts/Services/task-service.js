dbc9c7c0527b68f9120fe92ca0f62c2bc02d6ce0
robert <rhsu123@gmail.com>
2017-02-16 21:38:03 -0800

    indents'

--- AngularProto/Scripts/Services/task-service.js
+++ AngularProto/Scripts/Services/task-service.js
@@ -1,24 +1,24 @@
 (function () {
-	const app = angular.module('app');
+    const app = angular.module('app');
 
-	app.factory('TaskService', function ($http) {
-		
-		return {
-            
-			create(content) {
-				return $http.post('/Tasks/Create/', { content: content });
-			},
-            
-			retrieve() {
-				return $http.post('/Tasks/Retrieve/');
-			},
-            
+    app.factory('TaskService', function ($http) {
+
+        return {
+
+            create(content) {
+                return $http.post('/Tasks/Create/', { content: content });
+            },
+
+            retrieve() {
+                return $http.post('/Tasks/Retrieve/');
+            },
+
             update(task) {
                 return $http.post('/Tasks/Edit', { viewModel : task, 
                                                    taskId : task.taskId
                                                  });
             }
-		}
-	});
-    
+        }
+    });
+
 }());
--- AngularProto/Scripts/Services/task-table-view-service.js
+++ AngularProto/Scripts/Services/task-table-view-service.js
@@ -1,109 +1,109 @@
 (function() {
-	const app = angular.module('app');	
+    const app = angular.module('app');	
 
-	function TaskTableView(taskId, 
-		taskContent) {
-		this.taskId = taskId,
-		this.taskContent = taskContent,
+    function TaskTableView(taskId, 
+        taskContent) {
+        this.taskId = taskId,
+        this.taskContent = taskContent,
 
-		this.editorTaskContent = taskContent
+        this.editorTaskContent = taskContent
 
-		// valid editor states are:
-		// 'initial' : indicates loaded from the database
-		// 'confirmDelete' : indicates that user is about to delete it
-		// 'deleted' : indicates passed the confirmDelete state
-		// 'editing' : indicates that the task is in inline-editing mode
-		this.editorState = 'initial',
+        // valid editor states are:
+        // 'initial' : indicates loaded from the database
+        // 'confirmDelete' : indicates that user is about to delete it
+        // 'deleted' : indicates passed the confirmDelete state
+        // 'editing' : indicates that the task is in inline-editing mode
+        this.editorState = 'initial',
 
-		///////////////////////////////
-		//							//
-		// state change functions	//
-		//							//
-		//////////////////////////////
-		this.setInitialState = function () {
-			this.editorTaskContent = taskContent;
-			this.editorState = 'initial';
-		},
+        ///////////////////////////////
+        //							//
+        // state change functions	//
+        //							//
+        //////////////////////////////
+        this.setInitialState = function () {
+            this.editorTaskContent = taskContent;
+            this.editorState = 'initial';
+        },
 
-		this.setConfirmDeleteState = function () {
-			this.editorState = 'confirmDelete';
-		},
+        this.setConfirmDeleteState = function () {
+            this.editorState = 'confirmDelete';
+        },
 
-		this.setDeletedState = function () {
-			this.editorState = 'deleted';
-		},
+        this.setDeletedState = function () {
+            this.editorState = 'deleted';
+        },
 
-		this.setEditingState = function () {
-			this.editorState = 'editing';
-		}
-	}
+        this.setEditingState = function () {
+            this.editorState = 'editing';
+        }
+    }
 
-	//TODO: I would like to refactor this to something that indicates that it
-	//      is responsible to creating a {model} while invoking the {task-service}
-	//		What is an appropriate name for task-service to indicate
-	//		that is talks to the server via $http (like a repo).
-	//		Another thing we can do is simply differentiate these by folder structure
-	app.factory('TaskTableViewService', function (TaskService) {
+    //TODO: I would like to refactor this to something that indicates that it
+    //      is responsible to creating a {model} while invoking the {task-service}
+    //		What is an appropriate name for task-service to indicate
+    //		that is talks to the server via $http (like a repo).
+    //		Another thing we can do is simply differentiate these by folder structure
+    app.factory('TaskTableViewService', function (TaskService) {
 
-		return {
-			retrieve() {
-				return TaskService.retrieve().then(function (response) {
-					jsonObject = response.data;
+        return {
+            retrieve() {
+                return TaskService.retrieve().then(function (response) {
+                    jsonObject = response.data;
 
-					const tasks = [];
+                    const tasks = [];
 
-					for (let jsonObject of response.data) {
-						const task = new TaskTableView(jsonObject.TaskId,
-							jsonObject.TaskContent);
+                    for (let jsonObject of response.data) {
+                        const task = new TaskTableView(jsonObject.TaskId,
+                            jsonObject.TaskContent);
 
-						tasks.push(task);
-					}
+                        tasks.push(task);
+                    }
 
-					return tasks;
-				}, function (reason) {});
-			},
+                    return tasks;
+                }, function (reason) {});
+            },
 
-			create(taskData) {
-				return TaskService.create(taskData.taskContent).then(function (response) {
-					jsonObject = response.data;
+            create(taskData) {
+                return TaskService.create(taskData.taskContent).then(function (response) {
+                    jsonObject = response.data;
 
-					return new TaskTableView(jsonObject.TaskId,
-						jsonObject.TaskContent);
-				}, function (reason) {});
-			},
+                    return new TaskTableView(jsonObject.TaskId,
+                        jsonObject.TaskContent);
+                }, function (reason) {});
+            },
 
-			delete(task) {
+            delete(task) {
                 task.isDeleted = true;
-                
-				return TaskService.update(task).then(function (response) {
-					if (response.data) {
-						// TODO maybe the endpoint returns the entire task as is in the database
-						// then the client does something like 
-							// 1. makeTaskModelFromResponse
-							// 2. set task = the object from step 1
-						task.setDeletedState();
 
-						return task;
-					}
+                return TaskService.update(task).then(function (response) {
+                    if (response.data) {
+                        // TODO maybe the endpoint returns the entire task as is in the database
+                        // then the client does something like 
+                            // 1. makeTaskModelFromResponse
+                            // 2. set task = the object from step 1
+                        task.setDeletedState();
 
-				}, function (reason) {});
-			},
+                        return task;
+                    }
 
-			undoDelete(task) {
+                }, function (reason) {});
+            },
+
+            undoDelete(task) {
                 task.isDeleted = false;
-                
-				return TaskService.update(task).then(function (response) {
-					if (response.data) {
-						// TODO maybe the endpoint returns the entire task as is in the database
-						// then the client does something like 
-							// 1. makeTaskModelFromResponse
-							// 2. set task = the object from step 1
-						task.setInitialState();
 
-						return task;
-					}
-				}, function (reason) {});
-			}
-		}
-	});
+                return TaskService.update(task).then(function (response) {
+                    if (response.data) {
+                        // TODO maybe the endpoint returns the entire task as is in the database
+                        // then the client does something like 
+                            // 1. makeTaskModelFromResponse
+                            // 2. set task = the object from step 1
+                        task.setInitialState();
+
+                        return task;
+                    }
+                }, function (reason) {});
+            }
+        }
+    });
 })();
