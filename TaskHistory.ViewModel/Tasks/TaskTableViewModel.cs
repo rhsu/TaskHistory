1e03c4984c5b80506a3b30e02088d6f912bcaa4d
robert <rhsu123@gmail.com>
2017-02-21 10:08:32 -0800

    backup

--- TaskHistory.ViewModel/Tasks/TaskTableViewModel.cs
+++ TaskHistory.ViewModel/Tasks/TaskTableViewModel.cs
@@ -6,7 +6,9 @@
 		public string TaskContent { get; }
 
 		// TODO
-		// Add in IsDeleted
+		// Add in IsDeleted. 
+		// This might be necessary when updating the taskContent of a deletedTask
+		// or also allowing the user to view deleted tasks.
 
 		public TaskTableViewModel(int taskId, string taskContent)
 		{
