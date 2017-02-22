3bf02b7f85f6ad844cede20e4a898e388abdb450
robert <rhsu123@gmail.com>
2017-02-21 18:53:00 -0800

    controller + orchestrator

---
+++ TaskHistory.ObjectMapper/TaskLists/ObjectMapperTaskLists.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using TaskHistory.Api.TaskLists;
+using TaskHistory.ViewModel.TaskLists;
+
+namespace TaskHistory.ObjectMapper.TaskLists
+{
+	public class ObjectMapperTaskLists
+	{
+		public IEnumerable<TaskListViewModel> Map(IEnumerable<ITaskList> lists)
+		{
+			if (lists == null)
+				throw new ArgumentNullException(nameof(lists));
+
+			var viewModels = new List<TaskListViewModel>();
+
+			foreach (var list in lists)
+			{
+				var viewModel = Map(list);
+				viewModels.Add(viewModel);
+			}
+
+			return viewModels;
+		}
+
+		public TaskListViewModel Map(ITaskList list)
+		{
+			if (list == null)
+				throw new ArgumentNullException(nameof(list));
+
+			var viewModel = new TaskListViewModel();
+			viewModel.Id = list.Id;
+			viewModel.Name = list.Name;
+
+			return viewModel;
+		}
+	}
+}
---
+++ TaskHistory.Orchestrator/TaskListOrchestrator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using TaskHistory.Api.TaskLists;
+using TaskHistory.Api.Users;
+using TaskHistory.ObjectMapper.TaskLists;
+using TaskHistory.ViewModel.TaskLists;
+
+namespace TaskHistory.Orchestrator
+{
+	public class TaskListOrchestrator
+	{
+		readonly ITaskListRepo _repo;
+		readonly ObjectMapperTaskLists _mapper;
+
+		public IEnumerable<TaskListViewModel> Retrieve(IUser user)
+		{
+			if (user == null)
+				throw new ArgumentNullException(nameof(user));
+
+			var lists = _repo.Read(user.UserId);
+			if (lists == null)
+				throw new NullReferenceException("Null returned from repo");
+
+			var viewModels = _mapper.Map(lists);
+			if (viewModels == null)
+				throw new NullReferenceException("Null return from mapper");
+
+			return viewModels;
+		}
+
+		public TaskListViewModel Create(IUser user, string name)
+		{
+			if (user == null)
+				throw new ArgumentNullException(nameof(user));
+
+			if (string.IsNullOrEmpty(name))
+				throw new ArgumentNullException(nameof(name));
+
+			var list = _repo.Create(user.UserId, name);
+			if (list == null)
+				throw new NullReferenceException("null returned from repo");
+
+			var viewModel = _mapper.Map(list);
+			if (viewModel == null)
+				throw new NullReferenceException("null returned from mapper");
+
+			return viewModel;
+		}
+
+		public TaskListOrchestrator(ITaskListRepo repo,
+		                            ObjectMapperTaskLists mapper)
+		{
+			_repo = repo;
+			_mapper = mapper;
+		}
+	}
+}
