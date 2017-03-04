c5f112833969e8f338211cda750c692ae3b762fb
robert <rhsu123@gmail.com>
2017-03-04 13:29:44 -0800

    works

---
+++ AngularProto/Scripts/Controllers/lists-controller.js
@@ -0,0 +1,9 @@
+(function () {
+    'use strict';
+    
+    const app = angular.module('app');
+    
+    app.controller('ListsController', function ($scope){
+        $scope.data = 'this worked!';
+    });
+})();
