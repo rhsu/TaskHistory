ae7b7ad9d20bcdcec89ef627b0615ece8d6f7a8f
rhsu <rhsu123@gmail.com>
2017-03-07 09:50:57 -0800

    Task List UI (#255)

---
+++ AngularProto/Controllers/TaskListController.cs
@@ -0,0 +1,35 @@
+﻿using System.Web.Mvc;
+using TaskHistory.Orchestrator;
+
+namespace AngularProto.Controllers
+{
+	public class TaskListController : ApplicationController
+    {
+		TaskListOrchestrator _orchestrator;
+
+		[HttpGet]
+        public ActionResult Index()
+        {
+            return View ();
+        }
+
+		// TODO Might not need this
+		/*[HttpPost]
+		public JsonResult Retrieve()
+		{
+			return Json(_orchestrator.Retrieve(_currentUser));
+		}*/
+
+		[HttpPost]
+		public JsonResult Create(string name)
+		{
+			return Json(_orchestrator.Create(_currentUser, name));
+		}
+
+		public TaskListController(TaskListOrchestrator orchestrator, ApplicationContext appContext)
+			: base(appContext)
+		{
+			_orchestrator = orchestrator;
+		}
+    }
+}
