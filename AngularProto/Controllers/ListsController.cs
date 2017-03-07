0076217ec32c36c74e006dfdf1b0c2d2aec7adb3
rhsu <rhsu123@gmail.com>
2017-03-07 09:46:19 -0800

    Task list mock ui (#261)

---
+++ AngularProto/Controllers/ListsController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace AngularProto.Controllers
+{
+	public class ListsController : Controller
+    {
+		[HttpGet]
+        public ActionResult Index()
+        {
+            return View ();
+        }
+    }
+}
--- AngularProto/Scripts/app.js
+++ AngularProto/Scripts/app.js
@@ -16,6 +16,9 @@
 		.when('/Tasks', {
 			templateUrl: '/Tasks/Index'
 		})
+        .when('/Lists', {
+            templateUrl: '/Lists/Index'
+        })
 		.when('/Terminal', {
 			templateUrl: '/Tasks/Index'
 		});
