9f93b9fc301333f397a8605500c8b2bb8186c59d
robert <rhsu123@gmail.com>
2017-03-09 11:22:07 -0800

    usings

--- AngularProto/Controllers/AdminController.cs
+++ AngularProto/Controllers/AdminController.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace AngularProto.Controllers
 {
-    public class AdminController : Controller
-    {
-        public ActionResult Index()
-        {
-            return View ();
-        }
-    }
+	public class AdminController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+	}
 }
