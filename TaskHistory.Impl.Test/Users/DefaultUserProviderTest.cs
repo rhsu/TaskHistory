81f3cfb895079fd9ad6b6f578ba11cfeeb43e40e
rhsu <rhsu123@gmail.com>
2017-03-28 06:25:53 +0900

    Unit Tests for Default User Provider (#296)

--- TaskHistory.Impl.Test/Users/DefaultUserProviderTest.cs
+++ TaskHistory.Impl.Test/Users/DefaultUserProviderTest.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using TaskHistory.Impl.Sql;
 using TaskHistory.Impl.Users;
 
@@ -11,7 +10,7 @@ namespace TaskHistory.Impl.Test.Users
 		DefaultUserProvider _userProvider;
 		ApplicationDataProxy _appDataProxy;
 
-		void DeleteAllUsersUtil()
+		void DeleteAllUsers()
 		{
 			// TODO More proof that we need a mechanism to execute adhoc queries
 			// TODO This is fine for now but should delete later as that procedure
@@ -19,7 +18,22 @@ namespace TaskHistory.Impl.Test.Users
 			_appDataProxy.ExecuteNonQuery("USERS_ALL_DELETE");
 		}
 
+		[Test]
+		public void DefaultUserExists_False()
+		{
+			DeleteAllUsers();
 
+			Assert.False(_userProvider.DefaultUserExists());
+		}
+
+		[Test]
+		public void DefaultUserExists_True()
+		{
+			DeleteAllUsers();
+			_userProvider.RegisterDefaultUser();
+
+			Assert.True(_userProvider.DefaultUserExists());
+		}
 
 		public DefaultUserProviderTest()
 		{
@@ -28,7 +42,5 @@ namespace TaskHistory.Impl.Test.Users
 			var userRepo = new UserRepo(userFactory, _appDataProxy);
 			_userProvider = new DefaultUserProvider(userRepo);
 		}
-
-
 	}
 }
