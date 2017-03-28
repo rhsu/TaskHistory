3383b88af11cbab43d4461895bbc5ff9271c64cc
robert <rhsu123@gmail.com>
2017-03-28 14:21:39 -0700

    ready for review

--- TaskHistory.WebApp/Controllers/TaskListsController.cs
+++ TaskHistory.WebApp/Controllers/TaskListsController.cs
@@ -13,12 +13,10 @@ namespace TaskHistory.WebApp.Controllers
             return View ();
         }
 
-		// TODO Might not need this
 		[HttpPost]
 		public JsonResult Retrieve()
 		{
-			//return Json(_orchestrator.Retrieve(_currentUser));
-			return null;
+			return Json(_orchestrator.Read(_currentUser));
 		}
 
 		[HttpPost]
