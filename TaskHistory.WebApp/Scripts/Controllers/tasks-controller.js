75327d0914fbe42ecd73a5780682f61e94e9e3e0
rhsu <rhsu123@gmail.com>
2017-03-30 16:25:23 -0700

    Task table directive (#311)

--- TaskHistory.WebApp/Scripts/Controllers/tasks-controller.js
+++ TaskHistory.WebApp/Scripts/Controllers/tasks-controller.js
@@ -40,46 +40,5 @@
             }, function () {});
         };
 
-        $scope.pageFns.updateTaskContent = function (task) {
-          task.taskContent = task.editorTaskContent;
-          task.editorTaskContent = task.taskContent;
-          TaskService.update(task).then(function (response) {
-            const data = response.data;
-            if (data) {
-              TaskTableViewFactory.updateFromJson(data, task);
-              task.setInitialState();
-            }
-          }, function () {});
-        }
-
-        $scope.pageFns.deleteTask = function (task) {
-            task.isDeleted = true;
-            TaskService.update(task).then(function (response) {
-                const data = response.data;
-                if (data) {
-                    TaskTableViewFactory.updateFromJson(data, task);
-                    task.setDeletedState();
-                }
-            }, function () {});
-        };
-
-        $scope.pageFns.undoDeleteTask = function (task) {
-            task.isDeleted = false;
-            TaskService.update(task).then(function (response) {
-                const data = response.data;
-                if (data) {
-                    TaskTableViewFactory.updateFromJson(data, task);
-                    task.setInitialState();
-                }
-            }, function () {});
-        };
-
-        $scope.pageFns.displayBackButton = function (task) {
-            return (task.editorState === 'confirmDelete' || task.editorState === 'editing');
-        };
-
-        $scope.pageFns.displayReadonlyMode = function (task) {
-            return (task.editorState !== 'deleted' && task.editorState !== 'editing');
-        };
     });
 })();
