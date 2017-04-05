daf95ae501fd718450c5e14921f5d9292b835a22
robert <rhsu123@gmail.com>
2017-04-04 21:25:09 -0700

    purged more

--- TaskHistory.Api/TaskLists/ITaskListWithTasks.cs
+++ TaskHistory.Api/TaskLists/ITaskListWithTasks.cs
@@ -3,9 +3,6 @@ using TaskHistory.Api.Tasks;
 
 namespace TaskHistory.Api.TaskLists
 {
-	// TODO possibily have this inherit ITaskList
-	//      but currently it doesn't because I can't 
-	//      find a good reason for it to
 	public interface ITaskListWithTasks
 	{
 		int ListId { get; }
