6222a95d0e55d698ff365cb39ef22570b2b59e4b
robert <rhsu123@gmail.com>
2017-07-11 18:00:29 -0700

    created a base class for repos

---
+++ TaskHistory.Impl/Shared/BaseRepo.cs
@@ -0,0 +1,12 @@
+﻿namespace TaskHistory.Impl.Shared
+{
+	public abstract class BaseRepo
+	{
+		// TODO move this to a shared base
+		const string NullFromApplicationDataProxy = "Null returned from DataProvider";
+
+		public BaseRepo()
+		{
+		}
+	}
+}
