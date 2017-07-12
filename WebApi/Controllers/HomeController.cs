9ffb078eb775462b0dd030ff60a216c0d1321b10
robert <rhsu123@gmail.com>
2017-07-11 17:06:42 -0700

    didn't work

---
+++ WebApi/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Http;
+
+namespace WebApi.Controllers
+{
+	public class HomeController : ApiController
+	{
+		/*public ActionResult Index()
+		{
+			var mvcName = typeof(Controller).Assembly.GetName();
+			var isMono = Type.GetType("Mono.Runtime") != null;
+
+			ViewData["Version"] = mvcName.Version.Major + "." + mvcName.Version.Minor;
+			ViewData["Runtime"] = isMono ? "Mono" : ".NET";
+
+			return View();
+		}*/
+
+		public IHttpActionResult GetTest()
+		{
+			return Json("Hello World");
+		}
+	}
+}
