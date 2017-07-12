314f888e6fcd533eb27be5abb4ae99e0d9f6d2b6
robert <rhsu123@gmail.com>
2017-07-11 17:57:02 -0700

    backup

---
+++ TaskHistory.Api/Tasks/ITaskPriorityRepo.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskHistory.Api.Tasks
+{
+	public interface ITaskPriorityRepo
+	{
+		ITaskPriority Create();
+		ITaskPriority Read();
+		ITaskPriority Update();
+		ITaskPriority Delete();
+	}
+}
---
+++ TaskHistory.Impl/Tasks/TaskPriorityRepo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using TaskHistory.Api.Tasks;
+
+namespace TaskHistory.Impl.Tasks
+{
+	public class TaskPriorityRepo : ITaskPriorityRepo
+	{
+		public TaskPriorityRepo()
+		{
+		}
+
+		public ITaskPriority Create()
+		{
+			throw new NotImplementedException();
+		}
+
+		public ITaskPriority Delete()
+		{
+			throw new NotImplementedException();
+		}
+
+		public ITaskPriority Read()
+		{
+			throw new NotImplementedException();
+		}
+
+		public ITaskPriority Update()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
