ffaa4d2316b6cb16d82c2fa3c280cce9e04c70ee
robert <rhsu123@gmail.com>
2017-07-11 17:53:47 -0700

    added an implementation

---
+++ TaskHistory.Impl/Tasks/TaskPriority.cs
@@ -0,0 +1,31 @@
+﻿using TaskHistory.Api.Tasks;
+
+namespace TaskHistory.Impl
+{
+	public class TaskPriority : ITaskPriority
+	{
+		int _id;
+		string _name;
+		int _rank;
+		int _userId;
+
+		public int Id { get { return _id; } }
+
+		public string Name { get { return _name; } }
+
+		public int Rank { get { return _rank; } }
+
+		public int UserId { get { return _userId; } }
+
+
+		// TODO .NET now supports named parameter args
+		//		how do I do that?
+		public TaskPriority(int id, int userId, int rank, string name)
+		{
+			_id = id;
+			_userId = userId;
+			_name = name;
+			_rank = rank;
+		}
+	}
+}
