dd40da9ba820510e4e35b3571a0c578ebbc6156d
robert <rhsu123@gmail.com>
2017-07-19 08:19:00 -0700

    works

--- Database/TaskHistory.sql
+++ Database/TaskHistory.sql
@@ -672,6 +672,25 @@ DELIMITER ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
 /*!50003 SET character_set_results = @saved_cs_results */ ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `TaskPriority_Delete` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `TaskPriority_Delete`()
+BEGIN
+
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!50003 DROP PROCEDURE IF EXISTS `TaskPriority_Insert` */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
@@ -745,13 +764,16 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `TaskPriority_Update`(
 	`pId` INT,
     `pUserId` INT,
     `pName` VARCHAR(255),
-    `pRank` INT
+    `pRank` INT,
+    `pIsDeleted` bool
 )
 BEGIN
 	UPDATE `TaskPriority`
     SET
 		`Name` = `pName`,
-        `Rank` = `pRank`
+        `Rank` = `pRank`,
+        `IsDeleted` = `pIsDeleted`,
+        ModifiedDate = NOW()
 	WHERE
 		`Id` = `pId`
         AND `UserId` = `pUserId`;
@@ -1045,4 +1067,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-07-18 17:57:57
+-- Dump completed on 2017-07-19  8:17:46
