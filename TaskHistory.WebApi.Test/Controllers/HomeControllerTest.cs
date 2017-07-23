20a08cc75380664d50fcf23765af0666e3a93c23
rhsu <rhsu123@gmail.com>
2017-07-23 12:01:37 -0700

    Web api test 2 (#359)

---
+++ TaskHistory.WebApi.Test/Controllers/HomeControllerTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+using NUnit.Framework;
+using WebApi.Controllers;
+
+namespace TaskHistory.WebApi.Test.Controllers
+{
+	[TestFixture]
+	public class HomeControllerTest
+	{
+		[Test]
+		public void GetTest()
+		{
+			var controller = new HomeController();
+			controller.Request = new HttpRequestMessage();
+			controller.Configuration = new HttpConfiguration();
+
+			var response = controller.GetTest() as JsonResult<string>;
+			var content = response.Content;
+			Assert.AreEqual("Hello World", content);
+		}
+	}
+}
--- WebApi/Controllers/HomeController.cs
+++ WebApi/Controllers/HomeController.cs
@@ -1,23 +1,25 @@
-﻿using System.Web.Http;
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
 
 namespace WebApi.Controllers
 {
 	public class HomeController : ApiController
 	{
-		/*public ActionResult Index()
-		{
-			var mvcName = typeof(Controller).Assembly.GetName();
-			var isMono = Type.GetType("Mono.Runtime") != null;
-
-			ViewData["Version"] = mvcName.Version.Major + "." + mvcName.Version.Minor;
-			ViewData["Runtime"] = isMono ? "Mono" : ".NET";
-
-			return View();
-		}*/
-
 		public IHttpActionResult GetTest()
 		{
-			return Json("Hello World ABCDEFG");
+			return Json("Hello World");
+		}
+
+		// TODO this might be the correct syntax to use but 
+		//		I don't understand how it will interact with
+		//		Angular 2 yet.
+		//		In a Curl Test, I was not able to get a response
+		//		I am sticking with Json(_blah) until this is sorted out
+		public OkNegotiatedContentResult<string> Hello()
+		{
+			return Ok("Hello World");
 		}
 	}
 }
