5281c45eefd6f532f0e9c4af49f3c922d9f4fde7
robert <rhsu123@gmail.com>
2017-07-23 12:05:51 -0700

    need to migrate my testing framework over to WebApi

---
+++ TaskHistory.WebApi.Test/Controllers/TasksControllerTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace TaskHistory.WebApi.Test.Controllers.Tasks
+{
+	[TestFixture]
+	public class TasksControllerTest
+	{
+		[Test]
+		public void Get()
+		{
+			
+		}
+	}
+}
---
+++ WebApi/Controllers/TasksController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace WebApi.Controllers
+{
+	public class TaskController : Controller
+    {
+		public JsonResult Get()
+		{
+			
+			return Json(null);
+		}
+    }
+}
