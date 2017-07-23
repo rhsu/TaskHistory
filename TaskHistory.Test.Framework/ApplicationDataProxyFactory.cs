4b5d56da313efe448ccd85ccf4b776b196136c04
robert <rhsu123@gmail.com>
2017-07-23 12:12:55 -0700

    stuff

---
+++ TaskHistory.Test.Framework/ApplicationDataProxyFactory.cs
@@ -0,0 +1,22 @@
+﻿using TaskHistory.Impl.Configuration;
+using TaskHistory.Impl.Sql;
+
+namespace TaskHistory.TestFramework
+{
+	// TODO: Do I want to Keep this or replace this with
+	//       a  DI container?
+	public class ApplicationDataProxyFactory
+	{
+		public ApplicationDataProxy Build()
+		{
+			var sqlDataReaderFactory = new SqlDataReaderFactory();
+			var configurationProvider = new ConfigurationProvider();
+
+			var dataReaderProvider = new DataReaderProvider(sqlDataReaderFactory, configurationProvider);
+			var paramFactory = new SqlParameterFactory();
+			var nonQueryDataProvider = new NonQueryDataProvider(configurationProvider); 
+
+			return new ApplicationDataProxy(dataReaderProvider, paramFactory, nonQueryDataProvider);
+		}
+	}
+}
